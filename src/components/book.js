1e472e703845737191d79c2de003c2cf057a0a59
anbehindY <yanminthwin@gmail.com>
2023-05-06 14:18:16 +0630

    Display data from the prop passed in and set proptypes

--- src/components/book.js
+++ src/components/book.js
@@ -1,9 +1,11 @@
-const Book = () => (
+import PropTypes from 'prop-types';
+
+const Book = ({ bookInfo }) => (
   <div>
     <div className="description">
-      <p className="genre">Action</p>
-      <h3 className="title">Harry Potter</h3>
-      <small className="author">J.K.Rowling</small>
+      <p className="category">{bookInfo.category}</p>
+      <h3 className="title">{bookInfo.title}</h3>
+      <small className="author">{bookInfo.author}</small>
       <div className="btnContainer">
         <button type="button" className="comment">Comment</button>
         <button type="button" className="remove">Remove</button>
@@ -13,7 +15,7 @@ const Book = () => (
     <div className="progress">
       <img src="" alt="Progress circle" />
       <div className="completion">
-        <p className="percent">69%</p>
+        <p className="percent">{bookInfo.progress}</p>
         <small className="percentText">completed</small>
       </div>
     </div>
@@ -22,11 +24,21 @@ const Book = () => (
       <p>
         <span>Current chapter</span>
         <br />
-        Chapter 17
+        {bookInfo.chapter}
       </p>
       <button type="button" className="update">Update Progress</button>
     </div>
   </div>
 );
+
+Book.propTypes = {
+  bookInfo: PropTypes.shape({
+    category: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    progress: PropTypes.string.isRequired,
+    chapter: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default Book;
