446af75caa89eb410a0e1295be8a3ecb802758fa
anbehindY <yanminthwin@gmail.com>
2023-05-09 13:25:23 +0630

    Import reducers to the store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,12 @@
 import { configureStore } from '@testing-library/react';
+import bookReducer from './books/bookSlice';
+import categoryReducer from './category/categorySlice';
 
-const store = configureStore(
-    reducer: {
-        
-    },
-)
+const store = configureStore({
+  reducer: {
+    books: bookReducer,
+    categories: categoryReducer,
+  },
+});
+
+export default store;
