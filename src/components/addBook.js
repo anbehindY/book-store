abf67c514314b558fcb81ab39fb634f3bca945b2
anbehindY <yanminthwin@gmail.com>
2023-05-10 20:18:05 +0630

    Implement addBook action

--- src/components/addBook.js
+++ src/components/addBook.js
@@ -1,12 +1,29 @@
-const AddBook = () => (
-  <div className="bookInput">
-    <h3 className="addBook">ADD NEW BOOK</h3>
-    <form className="bookForm">
-      <input type="text" className="title" placeholder="Book title" />
-      <input type="text" className="author" placeholder="Author" />
-      <button type="submit" className="btnAdd">ADD BOOK</button>
-    </form>
-  </div>
-);
+import { useDispatch } from 'react-redux';
+import { nanoid } from '@reduxjs/toolkit';
+import { addBook } from '../redux/books/bookSlice';
+
+const AddBook = () => {
+  const dispatch = useDispatch();
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const book = {
+      title: e.target.title.value,
+      author: e.target.author.value,
+      item_id: nanoid(),
+    };
+    dispatch(addBook(book));
+  };
+
+  return (
+    <div className="bookInput">
+      <h3 className="addBook">ADD NEW BOOK</h3>
+      <form className="bookForm" onSubmit={submitHandler}>
+        <input type="text" className="title" name="title" placeholder="Book title" />
+        <input type="text" className="author" name="author" placeholder="Author" />
+        <button type="submit" className="btnAdd">ADD BOOK</button>
+      </form>
+    </div>
+  );
+};
 
 export default AddBook;
