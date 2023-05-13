ea27fcc9c14c10b0810fd420839a786bc7cc849f
anbehindY <yanminthwin@gmail.com>
2023-05-13 18:50:06 +0630

    Add styling to addBook form

--- src/components/categories.js
+++ src/components/categories.js
@@ -1,9 +1,11 @@
+import '../styles/categories.css';
+
 const Category = () => (
   <div className="ongoing">
     <p>
       Webpage building in progress...
       <br />
-      Please comeback later ;D
+      Please comeback later :D
     </p>
   </div>
 );
